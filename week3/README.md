ec82f0ca70a32c9b8e78f2ac4e499025509f1fc1
Carlos Pinto Machado <cpmachado@protonmail.com>
2021-04-22 19:54:30 +0100

    week3: exercise II

--- week3/README.md
+++ week3/README.md
@@ -4,3 +4,4 @@ Exercises from week3.
 
 List:
 1. [infinite-loop.c](infinite-loop.c)
+2. [char-conv.c](char-conv.c)
---
+++ week3/char-conv.c
@@ -0,0 +1,26 @@
+/* Copyright 2021 Carlos Pinto Machado */
+/* See LICENSE for details */
+
+/* HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int
+main(void) {
+    char v1[100];
+    char v2[100];
+    int i;
+
+    printf("Write a word: ");
+    fgets(v1, 100, stdin);
+
+    for (i = 0; v1[i] ; i++) {
+        v2[i] = toupper(v1[i]);
+    }
+
+    printf("Converted string: %s", v2);
+
+    return 0;
+}
