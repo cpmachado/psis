3d9f64db73f0f605d8bcb8c969ddffa1e832caa2
Carlos Pinto Machado <cpmachado@protonmail.com>
2021-04-22 19:48:01 +0100

    week3: exercise I

---
+++ week3/README.md
@@ -0,0 +1,6 @@
+# week3
+
+Exercises from week3.
+
+List:
+1. [infinite-loop.c](infinite-loop.c)
---
+++ week3/infinite-loop.c
@@ -0,0 +1,21 @@
+/* Copyright 2021 Carlos Pinto Machado */
+/* See LICENSE for details */
+
+/* HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int
+main(void) {
+    int v[100];
+    int i;
+
+    for (i = 0; i < 100; i += 1) {
+        v[i] = random();
+    }
+
+    printf("vector initialized at %p\n", v);
+
+    return 0;
+}
