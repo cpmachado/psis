8f9dc1c87a918f2a5778ca8345620879a52b3b2f
Carlos Pinto Machado <cpmachado@protonmail.com>
2021-04-22 22:12:18 +0100

    week4: exercise I

---
+++ week4/README.md
@@ -0,0 +1,6 @@
+# week4
+
+Exercises from week4.
+
+List:
+1. [cat-fork.c](cat-fork.c)
---
+++ week4/cat-fork.c
@@ -0,0 +1,30 @@
+/* Copyright 2021 Carlos Pinto Machado */
+/* See LICENSE for details */
+
+/* HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+/* FUNCTION DEFINITIONS */
+
+int
+main(void) {
+    char buffer[BUFSIZ];
+    pid_t pid;
+
+    while (fgets(buffer, BUFSIZ, stdin)) {
+        if ((pid = fork()) < 0) {
+            fprintf(stderr, "Error calling fork\n");
+            exit(EXIT_FAILURE);
+        } else if (!pid) {
+            printf("%s", buffer);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    return 0;
+}
+
