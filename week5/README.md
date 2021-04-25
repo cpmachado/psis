82990bba68bf891312658082004324144b821acc
Carlos Pinto Machado <cpmachado@protonmail.com>
2021-04-25 14:04:41 +0100

    week5: exercise III

--- week5/README.md
+++ week5/README.md
@@ -5,3 +5,4 @@ Exercises from week5.
 List:
 1. [sprintinc.c](sprintinc.c)
 2. [ten.c](ten.c)
+3. [tenv2.c](tenv2.c)
---
+++ week5/tenv2.c
@@ -0,0 +1,56 @@
+/* Copyright 2021 Carlos Pinto Machado */
+/* See LICENSE for details */
+
+/* HEADERS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+/* MACROS */
+#define N_THREADS 10
+
+
+/* FUNCTION DECLARATIONS */
+
+/* incrementThread: increment a given thread every random seconds */
+void* pidTidThread(void* data);
+
+
+/* FUNCTION DEFINITIONS */
+
+int
+main(void) {
+    pthread_t tid[N_THREADS];
+    unsigned long t[N_THREADS];
+    int i;
+    void *res;
+
+
+
+    for (i = 0; i < N_THREADS; i++) {
+        t[i] = random() % 11;
+        if (pthread_create(&tid[i], NULL, pidTidThread, (void*)&t[i])) {
+            fprintf(stderr, "Failed to create thread\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (i = 0; i < N_THREADS; i++) {
+        pthread_join(tid[i], &res);
+    }
+    return 0;
+}
+
+
+void*
+pidTidThread(void* data) {
+    unsigned long t = *(unsigned long*)data;
+
+    sleep(t);
+    printf("pid: %d, tid: %lu, t: %lu\n", getpid(), pthread_self(), t);
+
+    return NULL;
+}
