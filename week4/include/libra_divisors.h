b9c0e16f7a93f0cd14609b50693f8b38c37220e1
Carlos Pinto Machado <cpmachado@protonmail.com>
2021-04-25 14:46:53 +0100

    bugfix week4/include/libra_divisors.h

--- week4/include/libra_divisors.h
+++ week4/include/libra_divisors.h
@@ -9,6 +9,6 @@
 #define WEEK4_INCLUDE_LIBRA_DIVISORS_H_
 
 /* GLOBAL VARIABLES */
-static unsigned long divisors[] = { 2, 4, 5, 7};
+static unsigned long divisors[] = { 2, 3, 5, 7};
 
 #endif /* WEEK4_INCLUDE_LIBRA_DIVISORS_H_ */
